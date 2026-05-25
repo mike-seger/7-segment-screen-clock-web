54cbe5b1c7f3d34ecb36587c30602fbbaaa41d55
Michael Seger <michael@net128.com>
2026-05-26 00:25:15 +0200

    selected fonts optimization

--- configuration/cofiguration.js
+++ configuration/cofiguration.js
@@ -4,14 +4,18 @@
 
 const FONT_CORRECTION = {
     // AlarmClock: { size: 1.2, baseline: 0 },
-    DSEG7Classic: { colonMargin: 0.08 },
+    DSEG7Classic: { colonMargin: -0.3 },
+    DSEG7ClassicMini: { colonMargin: -0.3 },
     "Automata": { colonMarginLeft: -0.2, colonMargin: 0 },
     "Automate Regular W00 Regular": { colonMargin: -0.2 },
     // DSEG7ClassicMini: { size: 0.75, baseline: 0.06 },
-    // DSEG14Classic: { size: 0.75, baseline: 0.06 },
+    DSEG14Classic: { colonMargin: -0.3, o40: true },
     Digital7Mono: { colonMargin: -0.10, letterSpacing: 0.02, excludeMonoTweaks: true },
     // SevenSegment: { size: 1.1, baseline: -0.09 },
-    LCDDot: { baseline: 0.4, letterSpacing: 0.10 },
+    LCDDot: { o40: true },
+    MatrixSansRaster: { o40: true },
+    MatrixSansScreen: { o40: true },
+    RepetitionScrolling: { colonMarginLeft: -0.1, colonMargin: -0.2, excludeMonoTweaks: true  },
 };
 
 function normalizeFontName(value) {
