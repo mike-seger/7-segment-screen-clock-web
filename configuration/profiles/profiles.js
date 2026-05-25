5c03a335969feae565211ae4237d05c81f3e0e5f
Michael Seger <michael@net128.com>
2026-05-25 06:12:33 +0200

    profile handling

--- configuration/profiles/profiles.js
+++ configuration/profiles/profiles.js
@@ -15,7 +15,7 @@ window.BUILTIN_PROFILES = [
       "timeColor": "#04fb62",
       "secColor": "#00aaff",
       "secFontFactor": 0.63,
-      "showDebug": true
+      "showDebug": false
     }
   },
   {
@@ -34,7 +34,26 @@ window.BUILTIN_PROFILES = [
       "timeColor": "#2b4ac5",
       "secColor": "#f72222",
       "secFontFactor": 0.59,
-      "showDebug": true
+      "showDebug": false
+    }
+  },
+  {
+    "name": "Matrix",
+    "data": {
+      "numericFont": "LCDDot",
+      "alphaFont": "LCDDot",
+      "dualFont": true,
+      "numericScale": 100,
+      "alphaScale": 100,
+      "numericOffset": 0,
+      "alphaOffset": 0,
+      "weightGap": 0.19,
+      "fr": 0.23,
+      "dateColor": "#4de0bb",
+      "timeColor": "#f72222",
+      "secColor": "#f72222",
+      "secFontFactor": 0.59,
+      "showDebug": false
     }
   }
 ];
